463a330153a87e78b580c0bb8e053a4c85499944
BenBayotas <elijahben.bayotas@gmail.com>
2023-04-24 16:23:21 +0800

    Labeled GUI section

--- gui.py
+++ gui.py
@@ -1,8 +1,14 @@
 import tkinter
 from tkinter import *
 
+
+
+# System GUI--------------------------------------------------------------------------------
+
 root = Tk()
 title = root.title("BSCS Student Information System")
+
+
 
 root.mainloop()
 
