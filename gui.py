9297e4456d87f20a661adeee87e5b77430447945
BenBayotas <elijahben.bayotas@gmail.com>
2023-04-24 18:44:44 +0800

    Added new labels for entry

--- gui.py
+++ gui.py
@@ -7,13 +7,25 @@ from tkinter import *
 
 root = Tk()
 title = root.title("BSCS CRUD System")
-root.geometry('1280x720')
+root.geometry('1000x500')
 
 
-mainLabel = Label(root, text= "Enter Student Information", font=('Ariel', 15),)
-mainLabel.grid(row=1, column=0, padx=10, pady=10)
+mainLabel = Label(root, text= "Enter Student Information", font=('Ariel', 15))
+mainLabel.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
+
+idLabel = Label(root, text="Student Id", font=('Ariel', 13))
+lastnameLabel = Label(root, text="Last Name", font=('Ariel', 13))
+firstnameLabel = Label(root, text="First Name", font=('Ariel', 13))
+middlenameLabel = Label(root, text="Last Name", font=('Ariel', 13))
+courseLabel = Label(root, text="Course", font=('Ariel', 13))
+yearLabel = Label(root, text="Year Level", font=('Ariel', 13))
+
+idLabel.grid(row=3, column=0)
+lastnameLabel.grid(row=4, column=0)
+firstnameLabel.grid(row=5, column=0)
+middlenameLabel.grid(row=6, column=0)
+courseLabel.grid(row=7, column=0)
+yearLabel.grid(row=8, column=0)
 
 
 root.mainloop()
-
-
