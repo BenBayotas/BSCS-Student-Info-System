ae30f9f2a421b43c36885e9b033dc267eb95ce48
BenBayotas <elijahben.bayotas@gmail.com>
2023-04-24 16:47:33 +0800

    Added main label

--- gui.py
+++ gui.py
@@ -6,8 +6,12 @@ from tkinter import *
 # System GUI--------------------------------------------------------------------------------
 
 root = Tk()
-title = root.title("BSCS Student Information System")
+title = root.title("BSCS CRUD System")
+root.geometry('1280x720')
 
+
+mainLabel = Label(root, text= "Enter Student Information", font=('Ariel', 15),)
+mainLabel.grid(row=1, column=0, padx=10, pady=10)
 
 
 root.mainloop()
